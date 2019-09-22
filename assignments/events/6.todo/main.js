550b880fa6f7598dfd82e0a8b812a6d24f049a32
sunny <sunnychoudhury007@gmail.com>
2019-09-22 21:41:06 +0530

    update todo

--- assignments/events/6.todo/main.js
+++ assignments/events/6.todo/main.js
@@ -1,25 +1,8 @@
 let input = document.querySelector("input[type = text]");
-// console.dir(input);
-input.style.position = "relative";
 let ul = document.querySelector(".list");
-let footer1 = document.createElement("div");
-let f1 = document.createElement("div");
-f1.className = "f1";
-let f2 = document.createElement("div");
-f2.className = "f2";
-let f3 = document.createElement("div");
-f3.className ="f3";
-footer1.className = "footer1";
-let item = document.createElement("span");
-item.className = "item";
-let all = document.createElement("span");
-all.className = "all";
-let active = document.createElement("span");
-active.className = "active";
-let completed = document.createElement("span");
-completed.className = "completed";
-// console.dir(ul);
-// console.log(typeof(ul));
+let inputContainer = document.querySelector(".input_container");
+console.log(inputContainer);
+let liLength = document.querySelectorAll('.li_list').length;
 
 
 function handleSubmit (event){
@@ -29,46 +12,47 @@ function handleSubmit (event){
         const check = document.createElement("input");
         check.setAttribute("type", "checkbox");
         check.className = "check";
+        check.classList.add("checkRemove")
         const btn = document.createElement("i");
-        const li = document.createElement("li");  
+        const li = document.createElement("li"); 
+        li.className = 'li_list'; 
         btn.className = "far fa-times-circle";
         li.innerText = event.target.value;
-        input.value = "";
-        item.innerHTML = "1 <h3>item left</h3>";
-        all.textContent = "All";
-        active.textContent = "Active";
-        completed.textContent = "Completed";
+        document.querySelector(".input_container .fas").classList.add("fa-chevron-down");
+        document.querySelector(".foot").classList.add("footer_container");
         li.appendChild(btn);
         li.appendChild(check);
         ul.appendChild(li) ;
-        footer1.appendChild(item);
-        footer1.appendChild(all);
-        footer1.appendChild(active);
-        footer1.appendChild(completed);
-        ul.appendChild(footer1);
-        ul.appendChild(f1);
-        ul.appendChild(f2);
-        ul.appendChild(f3);
-       
+        event.target.value = "";
+        console.log(document.querySelectorAll('.li_list').length);
     }
 
-} 
+}    
 function deleteSubmit (event) {
     if(event.target.classList.contains('fa-times-circle')){
         event.target.parentElement.remove();
     }
 }
-function hover (event) {
-    if (event.target.classList.contains('fa-times-circle')){
-        event.target.style.color = "red";
+
+function handleCheck (event) {
+    if (event.target.classList.contains("check") && event.target.value === "on"){
+        event.target.parentElement.style.textDecoration = "line-through"
+        event.target.parentElement.style.color = "rgb(238, 231, 231)";
+        document.querySelector(".clear_completed").classList.add("clearCompleted");
+        for (i = 0; i < liLength ; i++ ){
+           document.querySelector(".item_count").innerText = i;
+        }
     }
 }
-function removeSpace (event) {
-    if (event.target.value.index[0]!=" "){
-        return event.target.value;
+
+function itemCount (event){
+    if (event.target.classList.contains("item_count")){
+
     }
 }
 ul.addEventListener("click" , deleteSubmit);
 document.addEventListener("keydown", handleSubmit);
-btn.addEventListener("mouseout",hover);
+ul.addEventListener("click",handleCheck);
+ul.addEventListener("click",handleUncheck);
+
 
